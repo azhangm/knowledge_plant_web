4d41969951948fe8907bae83adf273339cb2f7e9
da juancai[2~ <742824624@qq.com>
2022-08-24 00:22:06 +0800

    2-4  header 组件开发

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <TheHeader/>
   <router-view/>
     <a-layout-footer style="text-align: center">
       zm知识星球
     </a-layout-footer>
   </a-layout>
 </template>
+
+<script lang="ts">
+  import TheHeader from '@/components/the-header.vue'
+  export default {
+    components: {TheHeader}
+  }
+
+</script>
 
 <style>
 #components-layout-demo-top-side-2 .logo {
