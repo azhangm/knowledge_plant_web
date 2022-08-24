100e767ad40515511021ff7806bfdb443e7afeb8
da juancai[2~ <742824624@qq.com>
2022-08-24 08:01:39 +0800

    4-1 修改导航栏 路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import EbookManager from '@/views/admin/EbookManager.vue'
+import AboutMe from '@/views/AboutMe.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -8,13 +9,23 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
+    path: '/admin/ebook',
+    name: 'admin-ebook',
+    component: EbookManager
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: AboutMe
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/admin/EbookManager.vue')
+  // }
 ]
 
 const router = createRouter({
---
+++ src/views/AboutMe.vue
@@ -0,0 +1,10 @@
+<template>
+阿迪斯十大
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
