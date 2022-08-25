c61aab486cbf2bad19f3614de10250ece0bf47a7
da juancai[2~ <742824624@qq.com>
2022-08-25 20:09:37 +0800

    5-4 优化代码执行 解决网络慢数据出不来的问题

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -81,7 +81,6 @@ export default defineComponent({
       }).then((resp) => {
         console.log(resp);
         ebooks.value = resp.data.data.list;
-        // ebooks1.books = data.content;
       });
     };
     const level1 =  ref();
@@ -101,6 +100,8 @@ export default defineComponent({
           level1.value = [];
           level1.value = Tool.array2Tree(categorys, 0);
           console.log("树形结构：", level1.value);
+          handleQueryEbook();
+
         } else {
           message.error(data.message);
         }
@@ -110,7 +111,6 @@ export default defineComponent({
 
     onMounted(() => {
       handleQueryCategory();
-      handleQueryEbook();
     });
 
     return {
