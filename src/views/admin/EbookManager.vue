515ba036762f2d174dcd67c005c16518d8c09cd7
da juancai[2~ <742824624@qq.com>
2022-08-25 19:19:33 +0800

    5-4 增加级联选择器

--- src/views/admin/EbookManager.vue
+++ src/views/admin/EbookManager.vue
@@ -215,11 +215,12 @@ export default defineComponent({
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
-      // ebook.value.category1Id = categoryIds.value[0];
-      // ebook.value.category2Id = categoryIds.value[1];
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
+      console.log("ebook.value.category2Id" + ebook.value.category2Id);
       axios.put("/eBook/update", ebook.value).then((response) => {
         modalLoading.value = false;
-        const data = response.data; // data = commonResp
+        const data = response.data;
         if (data.success) {
           console.log(response);
           modalVisible.value = false;
@@ -240,13 +241,16 @@ export default defineComponent({
      */
     const edit = (record: any) => {
       modalVisible.value = true;
-      // categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
       ebook.value = Tool.copy(record) ;
+      console.log(record);
+      console.log(ebook.value);
+      categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
+
     };
     const handleModalAdd = () => {
       modalLoading.value = true;
-      // ebook.value.category1Id = categoryIds.value[0];
-      // ebook.value.category2Id = categoryIds.value[1];
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
       axios.post("/eBook/save", ebook.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
@@ -289,32 +293,32 @@ export default defineComponent({
 
     const level1 =  ref();
     let categorys: any;
-    // /**
-    //  * 查询所有分类
-    //  **/
-    // const handleQueryCategory = () => {
-    //   loading.value = true;
-    //   axios.get("/category/all").then((response) => {
-    //     loading.value = false;
-    //     const data = response.data;
-    //     if (data.success) {
-    //       categorys = data.content;
-    //       console.log("原始数组：", categorys);
-    //
-    //       level1.value = [];
-    //       level1.value = Tool.array2Tree(categorys, 0);
-    //       console.log("树形结构：", level1.value);
-    //
-    //       // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
-    //       handleQuery({
-    //         page: 1,
-    //         size: pagination.value.pageSize,
-    //       });
-    //     } else {
-    //       message.error(data.message);
-    //     }
-    //   });
-    // };
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/selectAll/").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          categorys = data.data;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+
+          // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+          handleQuery({
+            page: 1,
+            size: pagination.value.pageSize,
+          });
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
 
     const getCategoryName = (cid: number) => {
       // console.log(cid)
@@ -330,10 +334,11 @@ export default defineComponent({
 
 
     onMounted(() => {
-      // handleQuery({
-      //   page : 1,
-      //   size : pagination.value.pageSize
-      // });
+      handleQueryCategory();
+      handleQuery({
+        page : 1,
+        size : pagination.value.pageSize
+      });
     });
 
     return {
@@ -357,7 +362,7 @@ export default defineComponent({
       handleModalOk,
       categoryIds,
       level1,
-
+      handleQueryCategory,
       handleDelete
     }
   }
