ac6afc667f4d4fb16c759a710727f0b95db6b29d
da juancai[2~ <742824624@qq.com>
2022-08-25 15:48:36 +0800

    4-7  解决未更新响应数据bug

--- src/views/admin/EbookManager.vue
+++ src/views/admin/EbookManager.vue
@@ -117,7 +117,7 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
-
+import {Tool} from '@/assets/ts/tool'
 export default defineComponent({
   name: 'AdminEbook',
   setup() {
@@ -241,7 +241,7 @@ export default defineComponent({
     const edit = (record: any) => {
       modalVisible.value = true;
       // categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
-      ebook.value = record;
+      ebook.value = Tool.copy(record) ;
     };
     const handleModalAdd = () => {
       modalLoading.value = true;
